37d72e940ae1e93f61810a16fa6cafb6b3785def
98HMJ <good7min@naver.com>
2024-05-09 17:52:36 +0900

    HomeCont 제작

---
+++ src/main/java/dev/mvc/team4/HomeCont.java
@@ -0,0 +1,14 @@
+package dev.mvc.team4;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeCont {
+    //http://localhost:9093
+    //main.html 이동
+    @GetMapping("/")
+    public String home() { 
+        return "main";
+    }
+}
