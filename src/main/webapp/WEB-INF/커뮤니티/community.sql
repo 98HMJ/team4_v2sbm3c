6ec4401328a825364b3ec5600d1bd95ef48d19f8
98HMJ <good7min@naver.com>
2024-05-13 17:12:32 +0900

    회원가입 기능 제작 홍민재

--- src/main/webapp/WEB-INF/커뮤니티/community.sql
+++ src/main/webapp/WEB-INF/커뮤니티/community.sql
@@ -46,4 +46,13 @@ CREATE SEQUENCE community_seq
   INCREMENT BY 1            -- 증가값
   MAXVALUE 9999999999  -- 최대값: 9999999999 --> NUMBER(10) 대응
   CACHE 2                        -- 2번은 메모리에서만 계산
-  NOCYCLE;                      -- 다시 1부터 생성되는 것을 방지 
+  NOCYCLE;                      -- 다시 1부터 생성되는 것을 방지
+  
+--삽입--
+INSERT INTO community(communityno, title, contents, rdate, memberno, communitycateno) 
+VALUES(community_seq.nextval,'안녕하세요','처음 글을 씁니다.',sysdate,1,3);
+
+--조회--
+SELECT * FROM community;
+
+commit;
