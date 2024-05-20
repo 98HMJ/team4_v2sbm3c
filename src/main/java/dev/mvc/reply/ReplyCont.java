6671b65acf05e1da32880740537888e9ade8677a
byeong su kang <kbs95123@gmail.com>
2024-05-20 23:38:34 +0900

    Revert "application-secret.properties 파일"
    This reverts commit 57a87e35c8792731ec4595b9d87e3d1c3b63b8ea.

--- src/main/java/dev/mvc/reply/ReplyCont.java
+++ src/main/java/dev/mvc/reply/ReplyCont.java
@@ -17,20 +17,20 @@ import dev.mvc.member.MemberProc;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
-@RequestMapping("/reply")
+@RequestMapping("/reply") // Q. reply 를 requestmapping으로 주어도 적절한가?
 public class ReplyCont {
   @Autowired
   @Qualifier("dev.mvc.reply.ReplyProc")
   private ReplyProc replyProc;
 
   // Q. reqly 에 @Qualifier 로 연결할 ~Proc 은 무엇을 더 추가해야 하는가?
-  // community, replylike
-  
-  // Q. memberProc 가 꼭 필요한가?
+  // member, community, replylike
   @Autowired
   @Qualifier("dev.mvc.member.MemberProc")
   private MemberProc memberProc;  
 
+  // Q. 등록 절차에 필요한 작업은 무엇이 있는가?
+  // 1. 컨트롤러 제작 테스트 및 크리에이트 폼 데이터 처리
   public ReplyCont() {
     System.out.println("-> CateCont created.");
   }
@@ -42,40 +42,52 @@ public class ReplyCont {
   }
 
   // 등록 폼 처리
+  // 1. 등록 html 제작
+  // - 1. 정상적으로 등록 되는지 test
   // - 2. 댓글 등록 실패는 알림으로 대체
   @PostMapping(value = "/create") // http://localhost:9091/cate/create
   public String create(RedirectAttributes ra, 
                             HttpSession session, 
                             Model model, 
                             ReplyVO replyVO) {
-    if (session.getAttribute("id") != null) {
-      // System.out.println("-> communityno: "+ replyVO.getCommunityno());
+    System.out.println("-> communityno: "+ replyVO.getCommunityno());
       
-      model.addAttribute("contents", replyVO.getContents());
+    // form 에 등록된 text 를 가져오는 방법
+    // contents 가 text 로 등록이 되는지 확인
+    model.addAttribute("contents", replyVO.getContents());
+    
+    
+    // 1)커뮤니티 no 와 멤버 no 는 어떻게 가져올 것인지?
+    // * 커뮤니티 read 제작
+    // 커뮤니티 read.html의 form 태그 안에 
+    // <input type="hidden" name="communityno" th:value="${param.CommunityVO.communitino}">
+    // param 으로 커뮤니티 no 를 가져올 수 있는가?
+    replyVO.setCommunityno(replyVO.getCommunityno());
+    
+    // 2)멤버no 의 번호를  session 에서 받아옴
+    int memberno = (int) session.getAttribute("memberno");      
+    replyVO.setMemberno(memberno);
+    
+    // 3) 이미지 등록 처리
+    // - 파일 등록 처리하기
+    // model.addAttribute("imgs", replyVO.getPho());
+    // - advance) 우측에 드래앤 드롭으로 이미지 기능 제작
+    // - 어떻게?
+    int cnt = this.replyProc.create(replyVO);
+    System.out.println("-> cnt: "+ cnt);
       
-      replyVO.setCommunityno(replyVO.getCommunityno());
-      
-      int memberno = (int) session.getAttribute("memberno");      
-      replyVO.setMemberno(memberno);
-      
-      // 1. 이미지 등록 처리
-      // - 이미지 파일 등록 처리하기
-      // - 1) 이미지를 등록할 html 만들기
-      // model.addAttribute("imgs", replyVO.getPhoto());
-      // - advance) 우측에 드래앤 드롭으로 이미지 기능 제작
-      int cnt = this.replyProc.create(replyVO);
-      System.out.println("-> cnt: "+ cnt);
-        
 
-      model.addAttribute("cnt", cnt);
-      ra.addAttribute("communityno", replyVO.getCommunityno());
-      
-      return "redirect:/community/read"; // /templates/community/read.html 
-    }else {
-      model.addAttribute("code", "no_login");
-      return "member/login";
-    }
+    model.addAttribute("cnt", cnt);
+    ra.addAttribute("communityno", replyVO.getCommunityno());
+    
+    // 
+    return "redirect:/community/read"; // /templates/reply/create.html
   }
 
-  // 
+  // 2. 목록 제작
+  // 커뮤니티 read 에서 조회가 되는지 확인 작업 필요
+  // ArrayList<ReplyVO> list = this.replyProc.list_all();
+  
+  // 3. cnt 조회
+  // int cnt = this.replyProc.count_by_communityno(communityno);
 }
