a81c6b03cd4a48f3463b920184770877068d2fb4
98HMJ <good7min@naver.com>
2024-05-21 11:49:53 +0900

    사진 업로드 홍민재

--- src/main/java/dev/mvc/community/CommunityCont.java
+++ src/main/java/dev/mvc/community/CommunityCont.java
@@ -8,11 +8,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import dev.mvc.communitycate.CommunityCateProcInter;
 import dev.mvc.communitycate.CommunityCateVO;
 import dev.mvc.reply.ReplyProc;
 import dev.mvc.reply.ReplyVO;
+import dev.mvc.tool.Tool;
+import dev.mvc.tool.Upload;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -78,6 +81,44 @@ public class CommunityCont {
 
     @PostMapping("/create")
     public String create(CommunityVO communityVO, Model model, HttpSession session) {
+        // ------------------------------------------------------------------------------
+        // 파일 전송 코드 시작
+        // ------------------------------------------------------------------------------
+        String file1 = ""; // 원본 파일명 image
+        String file1saved = ""; // 저장된 파일명, image
+        String thumb1 = ""; // preview image
+
+        String upDir = Contents.getUploadDir(); // 파일을 업로드할 폴더 준비
+
+        // 전송 파일이 없어도 file1MF 객체가 생성됨.
+        // <input type='file' class="form-control" name='file1MF' id='file1MF'
+        // value='' placeholder="파일 선택">
+        MultipartFile mf = communityVO.getFile1MF();
+
+        file1 = mf.getOriginalFilename(); // 원본 파일명 산출, 01.jpg
+
+        long size1 = mf.getSize(); // 파일 크기
+        if (size1 > 0) { // 파일 크기 체크
+            if (Tool.checkUploadFile(file1) == true) { // 업로드 가능한 파일인지 검사
+                // 파일 저장 후 업로드된 파일명이 리턴됨, spring.jsp, spring_1.jpg...
+                file1saved = Upload.saveFileSpring(mf, upDir);
+
+                if (Tool.isImage(file1saved)) { // 이미지인지 검사
+                    // thumb 이미지 생성후 파일명 리턴됨, width: 200, height: 150
+                    thumb1 = Tool.preview(upDir, file1saved, 200, 150);
+                }
+
+                communityVO.setFiles(file1); // 순수 원본 파일명
+                communityVO.setFilesaved(file1saved); // 저장된 파일명(파일명 중복 처리)
+                communityVO.setThumb(thumb1); // 원본이미지 축소판
+                communityVO.setSize1(size1); // 파일 크기
+
+            } else { // 전송 못하는 파일 형식
+                model.addAttribute("code", "check_upload_file_fail"); // 업로드 할 수 없는 파일
+                return "member/msg"; // Post -> Get - param...
+            }
+        }
+
         communityVO.setMemberno((int) session.getAttribute("memberno"));
         int cnt = this.communityProc.create(communityVO);
         if (cnt == 1) {
