e44aed4c6ba78746baa507c5a4b6da10d0838e38
98HMJ <good7min@naver.com>
2024-06-04 13:15:53 +0900

    쓰레기 인코딩 오류 해결 홍민재

--- src/main/java/dev/mvc/team4/WebMvcConfiguration.java
+++ src/main/java/dev/mvc/team4/WebMvcConfiguration.java
@@ -1,18 +1,12 @@
 package dev.mvc.team4;
 
-import java.io.IOException;
-import java.net.URLDecoder;
-
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import dev.mvc.community.Community;
 import dev.mvc.reply.Reply;
 import dev.mvc.singo.Singo;
-import dev.mvc.trash.Trash;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
