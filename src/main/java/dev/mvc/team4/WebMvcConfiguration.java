2cdaf6eb70270bc2d4f4a5eba73784f348ef78b6
98HMJ <good7min@naver.com>
2024-06-04 11:08:50 +0900

    홍민재

--- src/main/java/dev/mvc/team4/WebMvcConfiguration.java
+++ src/main/java/dev/mvc/team4/WebMvcConfiguration.java
@@ -1,8 +1,13 @@
 package dev.mvc.team4;
 
+import java.io.IOException;
+import java.net.URLDecoder;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import dev.mvc.community.Community;
 import dev.mvc.reply.Reply;
@@ -10,29 +15,28 @@ import dev.mvc.singo.Singo;
 import dev.mvc.trash.Trash;
 
 @Configuration
-public class WebMvcConfiguration implements WebMvcConfigurer{
+public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Windows: path = "C:/kd/deploy/resort_v2sbm3c_blog/contents/storage";
-        // ▶ file:///C:/kd/deploy/resort_v2sbm3c_blog/contents/storage
-      
-        // Ubuntu: path = "/home/ubuntu/deploy/resort_v2sbm3c_blog/contents/storage";
-        // ▶ file:////home/ubuntu/deploy/resort_v2sbm3c_blog/contents/storage
-      
-        // JSP 인식되는 경로: http://localhost:9091/contents/storage";
-        //registry.addResourceHandler("/trash/storage/**").addResourceLocations("file:///" +  Trash.getUploadDir());
-        registry.addResourceHandler("/trash/storage/**").addResourceLocations("classpath:/static/images/trash/storage");
-        
-        registry.addResourceHandler("/replys/storage/**").addResourceLocations("file:///" +  Reply.getUploadDir());
-        
-        registry.addResourceHandler("/i/Desktop/teamfile/**").addResourceLocations("file:///" +  Community.getUploadDir());
-        
-        registry.addResourceHandler("/Desktop/teamfile/**").addResourceLocations("file:///" +  Singo.getUploadDir());
-        // JSP 인식되는 경로: http://localhost:9091/attachfile/storage";
-        // registry.addResourceHandler("/contents/storage/**").addResourceLocations("file:///" +  Tool.getOSPath() + "/attachfile/storage/");
-        
-        // JSP 인식되는 경로: http://localhost:9091/member/storage";
-        // registry.addResourceHandler("/contents/storage/**").addResourceLocations("file:///" +  Tool.getOSPath() + "/member/storage/");
-        
+        // registry.addResourceHandler("/trash/storage/**").addResourceLocations("classpath:/static/images/trash/storage");
+
+        registry.addResourceHandler("/replys/storage/**").addResourceLocations("file:///" + Reply.getUploadDir());
+
+        registry.addResourceHandler("/i/Desktop/teamfile/**")
+                .addResourceLocations("file:///" + Community.getUploadDir());
+
+        registry.addResourceHandler("/Desktop/teamfile/**").addResourceLocations("file:///" + Singo.getUploadDir());
+
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:/static/images/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                        resourcePath = URLDecoder.decode(resourcePath, "UTF-8");
+                        return super.getResource(resourcePath, location);
+                    }
+                });
+
     }
 }
