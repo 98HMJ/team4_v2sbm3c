ac45587cb6b21229620effac7be307576b9bad69
BPT0 <kbs95123@gmail.com>
2024-06-14 18:07:44 +0900

    ui. swiper 네비게이션 버튼 최적화 / 강병수

--- src/main/resources/static/js/modal.js
+++ src/main/resources/static/js/modal.js
@@ -1,103 +1,115 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  // expno 에 따라서 모달 창을 여는 함수
-  function loadDatas(expno) {
+  // expno 에 따라서 data를 로드하는 함수
+  async function loadDatas(expno) {
     const goUrl = '/trash_exploration/read?expno=' + expno;
 
-    fetch(goUrl, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-    })
-      .then(response => response.json())
-      .then(data => {
+    try {
+      const response = await fetch(goUrl, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+      });
 
-        // console.log(data.images);
-        var modalSwiperWrapper = document.getElementById('popupGallery').querySelector('.swiper-wrapper');
-        modalSwiperWrapper.innerHTML = ''; // 기존 슬라이드 제거
+      const data = await response.json();
 
-        data.images.forEach(imageUrl => {
-          // console.log(imageUrl);
-          var slideDiv = document.createElement('div');
-          slideDiv.className = 'swiper-slide';
-          slideDiv.innerHTML = `<img src="${imageUrl}">`;
-          modalSwiperWrapper.appendChild(slideDiv);
-        });
+      // console.log(data.images);
+      const modalSwiperWrapper = document.getElementById('popupGallery').querySelector('.swiper-wrapper');
+      modalSwiperWrapper.innerHTML = ''; // 기존 슬라이드 제거
 
-        popupGallery.update(); // Swiper 업데이트
+      data.images.forEach(imageUrl => {
+        // console.log(imageUrl);
+        const slideDiv = document.createElement('div');
+        slideDiv.className = 'swiper-slide';
+        slideDiv.innerHTML = `<img src="${imageUrl}">`;
+        modalSwiperWrapper.appendChild(slideDiv);
+      });
 
-      })
-      .catch(error => console.error('Error:', error));
+      // Swiper 초기화
+      initializeSwiper();
+
+    } catch (error) {
+      console.error('Error:', error);
+    }
   }
 
+  // 슬라이드의 위치에 따라 네비게이션 버튼의 표시 여부를 조정하는 함수
+  function checkNavButtons(swiper) {
+    const nextButton = swiper.el.querySelector('.custom-swiper-button-next');
+    const prevButton = swiper.el.querySelector('.custom-swiper-button-prev');
 
-  let popupGallery = new Swiper('#popupGallery', {
-    slidesPerView: 1, // 한 번에 표시할 슬라이드 수 1개
-    spaceBetween: 30, // 슬라이드 간의 간격 (30px)
-    centeredSlides: true, // 슬라이드 중앙 정렬
-    watchOverflow : true, // 슬라이드가 1개 일 때 pager, button 숨김 여부 설정
-    direction: 'horizontal', // 슬라이드 방향 (수평)
-    watchOverflow: true, // 슬라이드가 화면을 넘어갈 때의 처리 설정
+    if (swiper.isEnd) {
+      nextButton.style.display = 'none';
+    } else {
+      nextButton.style.display = 'block';
+    }
 
-    observer: true,
-    observeParents: true,
-  });
+    if (swiper.isBeginning) {
+      prevButton.style.display = 'none';
+    } else {
+      prevButton.style.display = 'block';
+    }
+  }
   
-  document.querySelector('.popupGalleryNext').addEventListener('click', function() {
-    popupGallery.slideNext();
-  });
-
-  document.querySelector('.popupGalleryPrev').addEventListener('click', function() {
-    popupGallery.slidePrev();
-  });
-
+  let popupGallery;
   var modalOpenButtons = document.querySelectorAll('.modal_open');
   const myModal = document.getElementById('myModal');
 
+  function initializeSwiper() {
+    if (popupGallery) {
+      popupGallery.destroy(true, true); // 기존 Swiper 인스턴스 정리
+    }
+
+    popupGallery = new Swiper('#popupGallery', {
+      slidesPerView: 1, // 한 번에 표시할 슬라이드 수 1개
+      spaceBetween: 30, // 슬라이드 간의 간격 (30px)
+      centeredSlides: true, // 슬라이드 중앙 정렬
+      direction: 'horizontal', // 슬라이드 방향 (수평)
+      observer: true,
+      observeParents: true,
+      navigation: {
+        nextEl: '.custom-swiper-button-next',
+        prevEl: '.custom-swiper-button-prev',
+      },
+      watchOverflow: true,
+      on: {
+        init: function() {
+          checkNavButtons(this);
+        },
+        slideChange: function() {
+          checkNavButtons(this);
+        }
+      },
+    });
+    
+    popupGallery.update(); // Swiper 업데이트
+  }
+  
   modalOpenButtons.forEach(function(button) {
     button.addEventListener('click', function(event) {
       event.preventDefault();
       
-      popupGallery = new Swiper('#popupGallery', {
-        slidesPerView: 1, // 한 번에 표시할 슬라이드 수 1개
-        spaceBetween: 30, // 슬라이드 간의 간격 (30px)
-        centeredSlides: true, // 슬라이드 중앙 정렬
-        watchOverflow : true, // 슬라이드가 1개 일 때 pager, button 숨김 여부 설정
-        direction: 'horizontal', // 슬라이드 방향 (수평)
-        watchOverflow: true, // 슬라이드가 화면을 넘어갈 때의 처리 설정
-    
-        observer: true,
-        observeParents: true,
-      });
-
       const expno = button.getAttribute('data-expno'); // expno 값을 가져옴
 
       loadDatas(expno);
-
+      
       // Show the modal
       const modalInstance = new bootstrap.Modal(myModal); // Ensure bootstrap.Modal is defined
       modalInstance.show();
 
-
       document.body.style.overflowY = "hidden"; // 스크롤 없애기
 
     });
-
-
   });
+
   
   // 모달이 보여질 때 Swiper 업데이트
   myModal.addEventListener('shown.bs.modal', function() {
-    popupGallery.update();
-  });
-  
-  // 모달 창을 닫는 버튼에 대한 JavaScript 코드
-  document.querySelector('.btn-close').addEventListener('click', function () {
-    myModal.hide();
-    document.body.style.overflowY = ""; // 스크롤 복원
+    // popupGallery.update();
   });
 
   // 모달이 닫힐 때 이벤트
   myModal.addEventListener('hidden.bs.modal', function () {
+    myModal.hide();
     document.body.style.overflowY = ""; // 스크롤 복원
   });
 
